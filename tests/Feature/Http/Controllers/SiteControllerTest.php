3102df7abe8c5ae44275f0b16f522d1ac2c52a11
Gustavo Specht <guspecht@hotmail.com>
2022-06-12 15:43:38 +0800

    Second Test done

--- tests/Feature/Http/Controllers/SiteControllerTest.php
+++ tests/Feature/Http/Controllers/SiteControllerTest.php
@@ -40,7 +40,26 @@ class SiteControllerTest extends TestCase
         $this->assertEquals($user->id, $site->user->id);
         // make sure see site name on the page
         $response->assertSeeText('Google');
+        // make sure we are in the right url
         $this->assertEquals(route('sites.show', $site), url()->current());
+    }
 
+
+    public function test_it_only_allow_authenticated_users_to_create_sites()
+    {
+        // user make a  post request to a route to create a site
+        $response = $this
+        ->followingRedirects()
+        ->post(route('sites.store'), [
+            'name' => 'Google',
+            'url' => 'https://www.google.com.au'
+        ]);
+        // make sure no site exists in the DB
+        $site  = Site::first();
+
+        // make sure see the login on the page, because we will be redirect to the login page
+        $response->assertSeeText('Log in');
+        // make sure we are in the login page
+        $this->assertEquals(route('login'), url()->current());
     }
 }
